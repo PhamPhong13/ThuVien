7db8397e5fcbdca37b06bb1d9c77b806c13e7617
PhamPhong13 <ptphonggithub@gmail.com>
2024-04-01 23:46:11 +0700

    Cai dat xu ly loi

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,16 @@ const cors = require('cors');
 
 const app = express();
 
+// router
 const Routerdongia = require('./app/routes/dongia.route');
 const Routernguoidung = require('./app/routes/nguoidung.route');
 const Routermuontra = require('./app/routes/muontra.route');
 const Routernhanvien = require('./app/routes/nhanvien.route');
 const Routernhaxuatban = require('./app/routes/nhaxuatban.route');
 const Routersach = require('./app/routes/sach.route');
+
+// class error
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +24,21 @@ app.use( "/api/nhanvien", Routernhanvien );
 app.use( "/api/nhaxuatban", Routernhaxuatban );
 app.use( "/api/sach", Routersach );
 
+// handle 404 response
+app.use((req, res, next) => {
+// Code ở đây sẽ chạy khi không có route được định nghĩa nào
+// khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+return next(new ApiError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+// Middleware xử lý lỗi tập trung.
+// Trong các đoạn code xử lý ở các route, gọi next(error)
+// sẽ chuyển về middleware xử lý lỗi này
+return res.status(err.statusCode || 500).json({
+message: err.message || "Internal Server Error",
+});
+});
 
 app.get("/", (req, res) => { 
     res.json({
