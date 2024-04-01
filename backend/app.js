5d9c67d26c2ef37960c54cf54ed2de25c9953be5
PhamPhong13 <ptphonggithub@gmail.com>
2024-04-01 23:19:53 +0700

    Cai dat express chay thong bao xin chao

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+// import thu vien
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => { 
+    res.json({
+        message: "Hello World!"
+    })
+})
+
+module.exports = app;
